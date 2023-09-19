a4752d509210fcaf5fb682ef42fa10ca8c7a63e0
LoweKumar <lowekr00@gmail.com>
2023-09-19 15:03:19 +0530

    commit msg 1 demo in my branch 1

--- app/email.py
+++ app/email.py
@@ -2,11 +2,15 @@ from threading import Thread
 from flask import current_app, render_template
 from flask_mail import Message
 from . import mail
-
+#My code
+def send_async_email(app, msg):
+    with app.app_context():
+       mail.send(msg)
 
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
+
 
 
 def send_email(to, subject, template, **kwargs):
